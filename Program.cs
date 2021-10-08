ea202dc0e8f8fc20b82807255c1c3316df2e148b
Ole I. Bruvik <oleib@me.com>
2021-10-08 09:59:35 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChkShtLst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
