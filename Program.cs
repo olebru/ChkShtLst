a4abea1ea6ef7abdad28efbdc7bc78feaf3ac98b
Ole I. Bruvik <oleib@me.com>
2021-10-27 12:50:12 +0200

    Messages

--- Program.cs
+++ Program.cs
@@ -9,18 +9,17 @@ namespace ChkShtLst
 
         static Task Main(string[] args)
         {
-            Console.WriteLine("Starting, looking for:");
-            foreach (var arg in args)
-	        {
-                Console.WriteLine(arg);
-	        }
+            Console.WriteLine("Starting");
+        
             bool foundWord = false;
             var runningDirName = Directory.GetCurrentDirectory();
             var errorMessage = new System.Text.StringBuilder();
             foreach (var arg in args)
             {
+                 Console.WriteLine($"Looking for: {arg}");
                 foreach (var fileName in Directory.GetFiles(runningDirName, "*", SearchOption.AllDirectories))
                 {
+                    Console.WriteLine($"\t in: {fileName}");
                     var fileContent = File.ReadAllText(fileName);
 
                     if (fileContent.Contains(arg))
